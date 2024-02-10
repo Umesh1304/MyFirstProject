c8bd383e646b49551b81179bd43b1f0d0c9dd611
unknown <umeshluffy@gmail.com>
2024-02-10 16:55:42 +0530

    Addinggg the  prct

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function displayValue(val) {
+    document.getElementById("display").value = document.getElementById("display").value + val;
+
+}
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+function calculate() {
+
+    var userInput = document.getElementById("display").value;
+    var result=eval(userInput);
+
+    document.getElementById("display").value=result;
+    
+}
